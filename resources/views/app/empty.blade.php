e23382ee000d14a5010e4c78dbb85b53e35f34a7
rdehnhardt <renatotkd@gmail.com>
2015-07-06 10:22:23 -0300

    meta noindex

--- resources/views/app/empty.blade.php
+++ resources/views/app/empty.blade.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="robots" content="noindex">
+
     <title>Deployer</title>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="https://bootswatch.com/united/bootstrap.min.css"/>
     <link rel="stylesheet" href="/css/app.css"/>
+
     <script src="/js/modernizr.js"></script>
 </head>
 
