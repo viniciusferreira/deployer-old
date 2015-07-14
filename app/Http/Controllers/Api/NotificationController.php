82e246451115dc87272da3994c49bbc51a626093
rdehnhardt <renatotkd@gmail.com>
2015-07-14 18:36:17 -0300

    Init API Controllers

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
 
-class HeartbeatController extends Controller
+class NotificationController extends Controller
 {
 
     /**
