f6ebda8582e961007d3f68637f623af814dd71fd
rdehnhardt <renatotkd@gmail.com>
2015-07-14 18:43:34 -0300

    Init API Controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,8 +10,11 @@ Route::group(['prefix' => 'api', 'namespace' => 'Api'], function () {
     Route::resource('project.collaborator', 'CollaboratorController');
 });
 
-Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 
+
+
+
+Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 Route::get('/projects/{project}', ['as' => 'project', 'uses' => 'ProjectController@show']);
 Route::get('/projects/{project}/health', ['as' => 'project.health', 'uses' => 'ProjectController@health']);
 Route::get('/projects/{project}/settings', ['as' => 'project.settings', 'uses' => 'ProjectController@settings']);
